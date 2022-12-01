017da79ef169a3fdfecc1c49904b07a9db2cbd08
Mathieu <mathieuveron@outlook.fr>
2022-12-02 00:07:40 +0100

    black test

--- tests/test_class_define.py
+++ tests/test_class_define.py
@@ -7,6 +7,7 @@ import pytest
 import sympy as sp
 from adap_exercice.class_define import Profit
 
+
 def test_quantity_1():
     quantity = Profit().quantity_1()
     qualite_1 = sp.symbols("s_1")
@@ -14,41 +15,47 @@ def test_quantity_1():
     attendu = qualite_1 - prix_1
     assert quantity == attendu
 
+
 def test_quantity_2():
     quantity = Profit().quantity_2()
     qualite_2 = sp.symbols("s_2")
     prix_2 = sp.symbols("p_2")
     attendu = qualite_2 - prix_2
     assert quantity == attendu
-    
+
+
 def test_cost_2_NDV():
-    cost_2_NDV = Profit().cost_2_NDV() 
-    gamma = sp.symbols("gamma") 
+    cost_2_NDV = Profit().cost_2_NDV()
+    gamma = sp.symbols("gamma")
     quantite_1 = sp.symbols("q_1")
-    attendu = 1 - gamma*quantite_1
+    attendu = 1 - gamma * quantite_1
     assert cost_2_NDV == attendu
+
 
 def test_cost_2():
     cost_2 = Profit().cost_2()
-    gamma = sp.symbols("gamma") 
+    gamma = sp.symbols("gamma")
     qualite_1 = sp.symbols("s_1")
     prix_1 = sp.symbols("p_1")
-    attendu = 1 - gamma*(qualite_1 - prix_1)
+    attendu = 1 - gamma * (qualite_1 - prix_1)
     assert cost_2 == attendu
+
 
 def test_profit_1():
     profit = Profit().profit_1()
     qualite_1 = sp.symbols("s_1")
     prix_1 = sp.symbols("p_1")
-    attendu = (qualite_1 - prix_1)*(prix_1 - 1)
+    attendu = (qualite_1 - prix_1) * (prix_1 - 1)
     assert profit == attendu
+
 
 def test_profit_1_NDV():
     profit = Profit().profit_1_NDV()
     quantite_1 = sp.symbols("q_1")
     prix_1 = sp.symbols("p_1")
-    attendu = (quantite_1)*(prix_1 - 1)
+    attendu = (quantite_1) * (prix_1 - 1)
     assert profit == attendu
+
 
 def test_profit_2():
     profit = Profit().profit_2()
@@ -56,8 +63,9 @@ def test_profit_2():
     qualite_2 = sp.symbols("s_2")
     prix_2 = sp.symbols("p_2")
     gamma = sp.symbols("gamma")
-    attendu = (qualite_2 - prix_2)*(prix_2 - (1- gamma*quantite_1))
+    attendu = (qualite_2 - prix_2) * (prix_2 - (1 - gamma * quantite_1))
     assert profit == attendu
+
 
 def test_profit_2_NDV():
     profit = Profit().profit_2_NDV()
@@ -65,8 +73,9 @@ def test_profit_2_NDV():
     quantite_2 = sp.symbols("q_2")
     prix_2 = sp.symbols("p_2")
     gamma = sp.symbols("gamma")
-    attendu = quantite_2*(prix_2 - (1- gamma*quantite_1))
+    attendu = quantite_2 * (prix_2 - (1 - gamma * quantite_1))
     assert profit == attendu
+
 
 def test_profit_2_FULL():
     profit = Profit().profit_2_FULL()
@@ -76,5 +85,7 @@ def test_profit_2_FULL():
     qualite_1 = sp.symbols("s_1")
     prix_1 = sp.symbols("p_1")
     qualite_2 = sp.symbols("s_2")
-    attendu = attendu = (qualite_2 - prix_2)*(prix_2 - (1- gamma*(qualite_1-prix_1)))
+    attendu = attendu = (qualite_2 - prix_2) * (
+        prix_2 - (1 - gamma * (qualite_1 - prix_1))
+    )
     assert profit == attendu
--- tests/test_resolution.py
+++ tests/test_resolution.py
@@ -7,161 +7,212 @@ import pytest
 import sympy as sp
 from sympy import Rational
 from adap_exercice.class_define import Profit
-from adap_exercice.resolution import (cn1_prof2,cs2_prof2,resolution_2,substitute_q2,profit_equilibre,resol_prof2_H,resol_prof2_L,resol_prof2subq_LL,resol_prof2subq_HH,resol_prof2subq_LH,resol_prof2subq_HL,resol_prof1_H,resol_prof1_L,resol_prof_HH,resol_prof_LL,resol_prof_HL,resol_prof_LH,cn1_profLL,cs2_profLL,prix_equilibre_LL,profit_equilibre_LL,prof_HL_A,form_A,opt_A, graph_A, roots_A)
+from adap_exercice.resolution import (
+    cn1_prof2,
+    cs2_prof2,
+    resolution_2,
+    substitute_q2,
+    profit_equilibre,
+    resol_prof2_H,
+    resol_prof2_L,
+    resol_prof2subq_LL,
+    resol_prof2subq_HH,
+    resol_prof2subq_LH,
+    resol_prof2subq_HL,
+    resol_prof1_H,
+    resol_prof1_L,
+    resol_prof_HH,
+    resol_prof_LL,
+    resol_prof_HL,
+    resol_prof_LH,
+    cn1_profLL,
+    cs2_profLL,
+    prix_equilibre_LL,
+    profit_equilibre_LL,
+    prof_HL_A,
+    form_A,
+    opt_A,
+    graph_A,
+    roots_A,
+)
 from unittest.mock import patch
 from matplotlib.testing.decorators import image_comparison
 
 
 def test_cn1_prof2():
     profit = Profit()
-    observe = sp.latex(cn1_prof2(profit,profit.p_2))
-    attendu = '- \\gamma q_{1} - 2 p_{2} + s_{2} + 1'
+    observe = sp.latex(cn1_prof2(profit, profit.p_2))
+    attendu = "- \\gamma q_{1} - 2 p_{2} + s_{2} + 1"
     assert observe == attendu
+
 
 def test_cs2_prof2():
     profit = Profit()
-    observe = sp.latex(cs2_prof2(profit,profit.p_2))
-    attendu = '-2'
+    observe = sp.latex(cs2_prof2(profit, profit.p_2))
+    attendu = "-2"
     assert observe == attendu
+
 
 def test_resolution_2():
     profit = Profit()
-    observe = sp.latex(resolution_2(profit,profit.p_2))
-    attendu = '- \\frac{\\gamma q_{1}}{2} + \\frac{s_{2}}{2} + \\frac{1}{2}'
+    observe = sp.latex(resolution_2(profit, profit.p_2))
+    attendu = "- \\frac{\\gamma q_{1}}{2} + \\frac{s_{2}}{2} + \\frac{1}{2}"
     assert observe == attendu
+
 
 def test_substitute_q2():
     profit = Profit()
-    observe = sp.latex(substitute_q2(profit,profit.p_2))
-    attendu = '\\frac{\\gamma q_{1}}{2} + \\frac{s_{2}}{2} - \\frac{1}{2}'
-    assert observe == attendu 
+    observe = sp.latex(substitute_q2(profit, profit.p_2))
+    attendu = "\\frac{\\gamma q_{1}}{2} + \\frac{s_{2}}{2} - \\frac{1}{2}"
+    assert observe == attendu
+
 
 def test_profit_equilibre():
     profit = Profit()
-    observe = sp.latex(profit_equilibre(profit,profit.p_2))
-    attendu = '\\frac{\\left(\\gamma q_{1} + s_{2} - 1\\right)^{2}}{4}'
+    observe = sp.latex(profit_equilibre(profit, profit.p_2))
+    attendu = "\\frac{\\left(\\gamma q_{1} + s_{2} - 1\\right)^{2}}{4}"
     assert observe == attendu
+
 
 def test_resol_prof2_H():
     profit = Profit()
-    observe = sp.latex(resol_prof2_H(profit,profit.p_2))
-    attendu = '\\frac{\\left(q_{1} + 12\\right)^{2}}{64}'
+    observe = sp.latex(resol_prof2_H(profit, profit.p_2))
+    attendu = "\\frac{\\left(q_{1} + 12\\right)^{2}}{64}"
     assert attendu == observe
+
 
 def test_resol_prof2_L():
     profit = Profit()
-    observe = sp.latex(resol_prof2_L(profit,profit.p_2))
-    attendu = '\\frac{\\left(q_{1} + 2\\right)^{2}}{16}'
+    observe = sp.latex(resol_prof2_L(profit, profit.p_2))
+    attendu = "\\frac{\\left(q_{1} + 2\\right)^{2}}{16}"
     assert attendu == observe
+
 
 def test_resol_prof2subq_LL():
     profit = Profit()
-    observe = sp.latex(resol_prof2subq_LL(profit,profit.p_2))
-    attendu = '\\frac{\\left(p_{1} - 4\\right)^{2}}{16}'
+    observe = sp.latex(resol_prof2subq_LL(profit, profit.p_2))
+    attendu = "\\frac{\\left(p_{1} - 4\\right)^{2}}{16}"
     assert observe == attendu
+
 
 def test_resol_prof2subq_HH():
     profit = Profit()
-    observe = sp.latex(resol_prof2subq_HH(profit,profit.p_2))
-    attendu = '\\frac{\\left(p_{1} - 16\\right)^{2}}{64}'
+    observe = sp.latex(resol_prof2subq_HH(profit, profit.p_2))
+    attendu = "\\frac{\\left(p_{1} - 16\\right)^{2}}{64}"
     assert observe == attendu
+
 
 def test_resol_prof2subq_LH():
     profit = Profit()
-    observe = sp.latex(resol_prof2subq_LH(profit,profit.p_2))
-    attendu = '\\frac{\\left(p_{1} - 14\\right)^{2}}{64}'
+    observe = sp.latex(resol_prof2subq_LH(profit, profit.p_2))
+    attendu = "\\frac{\\left(p_{1} - 14\\right)^{2}}{64}"
     assert observe == attendu
+
 
 def test_resol_prof2subq_HL():
     profit = Profit()
-    observe = sp.latex(resol_prof2subq_HL(profit,profit.p_2))
-    attendu = '\\frac{\\left(p_{1} - 4\\right)^{2}}{16}'
+    observe = sp.latex(resol_prof2subq_HL(profit, profit.p_2))
+    attendu = "\\frac{\\left(p_{1} - 4\\right)^{2}}{16}"
     assert observe == attendu
+
 
 def test_resol_prof1_H():
     profit = Profit()
-    observe = sp.latex(resol_prof1_H(profit,profit.q_1_NDV))
-    attendu = '\\left(- p_{1} + q_{1}\\right) \\left(p_{1} - 1\\right)'
+    observe = sp.latex(resol_prof1_H(profit, profit.q_1_NDV))
+    attendu = "\\left(- p_{1} + q_{1}\\right) \\left(p_{1} - 1\\right)"
     assert observe == attendu
+
 
 def test_resol_prof1_L():
     profit = Profit()
-    observe = sp.latex(resol_prof1_L(profit,profit.q_1_NDV))
-    attendu = '\\left(- p_{1} + q_{1}\\right) \\left(p_{1} - 1\\right)'
+    observe = sp.latex(resol_prof1_L(profit, profit.q_1_NDV))
+    attendu = "\\left(- p_{1} + q_{1}\\right) \\left(p_{1} - 1\\right)"
     assert observe == attendu
-    
+
+
 def test_resol_prof_HH():
     profit = Profit()
-    observe = sp.latex(resol_prof_HH(profit,profit.p_2))
-    attendu = '- \\frac{63 p_{1}^{2}}{64} + \\frac{9 p_{1}}{2}'
+    observe = sp.latex(resol_prof_HH(profit, profit.p_2))
+    attendu = "- \\frac{63 p_{1}^{2}}{64} + \\frac{9 p_{1}}{2}"
     assert observe == attendu
+
 
 def test_resol_prof_LL():
     profit = Profit()
-    observe = sp.latex(resol_prof_LL(profit,profit.p_2))
-    attendu = '- \\frac{15 p_{1}^{2}}{16} + \\frac{5 p_{1}}{2} - 1'
+    observe = sp.latex(resol_prof_LL(profit, profit.p_2))
+    attendu = "- \\frac{15 p_{1}^{2}}{16} + \\frac{5 p_{1}}{2} - 1"
     assert observe == attendu
+
 
 def test_resol_prof_HL():
     profit = Profit()
-    observe = sp.latex(resol_prof_HL(profit,profit.p_2))
-    attendu = '- \\frac{15 p_{1}^{2}}{16} + \\frac{17 p_{1}}{4} - \\frac{7}{4}'
+    observe = sp.latex(resol_prof_HL(profit, profit.p_2))
+    attendu = "- \\frac{15 p_{1}^{2}}{16} + \\frac{17 p_{1}}{4} - \\frac{7}{4}"
     assert observe == attendu
+
 
 def test_resol_prof_LH():
     profit = Profit()
-    observe = sp.latex(resol_prof_LH(profit,profit.p_2))
-    attendu = '- \\frac{63 p_{1}^{2}}{64} + \\frac{41 p_{1}}{16} + \\frac{17}{16}'
+    observe = sp.latex(resol_prof_LH(profit, profit.p_2))
+    attendu = "- \\frac{63 p_{1}^{2}}{64} + \\frac{41 p_{1}}{16} + \\frac{17}{16}"
     assert observe == attendu
+
 
 def test_cn1_profLL():
-    profit =Profit()
+    profit = Profit()
     observe = sp.latex(cn1_profLL(profit))
-    attendu = '\\frac{5}{2} - \\frac{15 p_{1}}{8}'
+    attendu = "\\frac{5}{2} - \\frac{15 p_{1}}{8}"
     assert observe == attendu
 
+
 def test_cs2_profLL():
-    profit =Profit()
+    profit = Profit()
     observe = sp.latex(cs2_profLL(profit))
-    attendu = '- \\frac{15}{8}'
+    attendu = "- \\frac{15}{8}"
     assert observe == attendu
+
 
 def test_profit_equilibre_LL():
     profit = Profit()
     observe = sp.latex(profit_equilibre_LL(profit))
-    attendu = '\\frac{2}{3}'
-    assert observe == attendu 
+    attendu = "\\frac{2}{3}"
+    assert observe == attendu
+
 
 def test_prix_equilibre_LL():
     profit = Profit()
     observe = sp.latex(prix_equilibre_LL)
-    attendu = '\\frac{8}{3}'
+    attendu = "\\frac{8}{3}"
+
 
 def test_prof_HL_A():
     profit = Profit()
-    observe = sp.latex(prof_HL_A(profit,profit.p_2))
-    attendu = '- A - \\frac{15 p_{1}^{2}}{16} + \\frac{17 p_{1}}{4} - \\frac{7}{4}'
+    observe = sp.latex(prof_HL_A(profit, profit.p_2))
+    attendu = "- A - \\frac{15 p_{1}^{2}}{16} + \\frac{17 p_{1}}{4} - \\frac{7}{4}"
     assert observe == attendu
-    
+
+
 def test_form_A():
     profit = Profit()
-    observe = sp.latex(form_A(profit,profit.p_2))
-    attendu = '- \\frac{15 p_{1}^{2}}{16} + \\frac{17 p_{1}}{4} - \\frac{29}{12}'
+    observe = sp.latex(form_A(profit, profit.p_2))
+    attendu = "- \\frac{15 p_{1}^{2}}{16} + \\frac{17 p_{1}}{4} - \\frac{29}{12}"
     assert observe == attendu
 
 
 def test_opt_A():
     profit = Profit()
-    observe = sp.latex(opt_A(profit,profit.p_2))
-    attendu = '\\frac{8}{3}'
+    observe = sp.latex(opt_A(profit, profit.p_2))
+    attendu = "\\frac{8}{3}"
     assert observe == attendu
+
 
 def test_graph_A():
     profit = Profit()
-    graph_A(profit,profit.p_2)
+    graph_A(profit, profit.p_2)
+
 
 def test_roots_A():
     profit = Profit()
-    observe = roots_A(profit,profit.p_2)
-    attendu = 'There are two roots to $A(p_1)$, he makes advertising only inside the interval formed by the two roots :$\\frac{2}{3}$ and $\\frac{58}{15}$'
+    observe = roots_A(profit, profit.p_2)
+    attendu = "There are two roots to $A(p_1)$, he makes advertising only inside the interval formed by the two roots :$\\frac{2}{3}$ and $\\frac{58}{15}$"
     assert observe == attendu
